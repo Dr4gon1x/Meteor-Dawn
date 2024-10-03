4dc1d33baf263571125c0b7de8ff83d6720acaac
DoktorBamse <doktorbamse2620@gmail.com>
2024-10-03 10:29:57 +0200

    Changed script name and player pos
    I changed the player spawn position to be in middle on top of the planet. And i changed the "meteorCollider" til "playerCollider"

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeteorCollider : MonoBehaviour
+public class PlayerCollider : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
