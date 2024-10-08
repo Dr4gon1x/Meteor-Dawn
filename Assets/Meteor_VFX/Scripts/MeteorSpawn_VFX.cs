044df0fe46a0cc8459395f7ad4b0b6b1f01858bc
MoonShadow <kean05olsen@gmail.com>
2024-10-08 22:07:57 +0200

    Lidt pæner men ikke perfekt meteor regn

--- Assets/Meteor_VFX/Scripts/MeteorSpawn_VFX.cs
+++ Assets/Meteor_VFX/Scripts/MeteorSpawn_VFX.cs
@@ -7,12 +7,16 @@ public class MeteorSpawn_VFX : MonoBehaviour
     public GameObject vfx;
     public Transform startPoint;
     public Transform endPoint;
-    public float spawnRange = 700f; // Range around the start point to spawn meteors
+    public float spawnRange = 600f; // Range around the start point to spawn meteors
+    public float moveSpeed = 100f; // Speed at which meteors move
+    public GameObject impactPregab;
 
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        SpawnMeteors(50);
+        SpawnMeteors(15);
+        rb = GetComponent<Rigidbody>();
     }
 
     void RotateTo(GameObject obj, Vector3 destination)
@@ -38,6 +42,35 @@ public class MeteorSpawn_VFX : MonoBehaviour
             var endPos = endPoint.position;
 
             RotateTo(vfxObj, endPos);
+            StartCoroutine(MoveMeteor(vfxObj, startPos, endPos));
+        }
+    }
+
+    IEnumerator MoveMeteor(GameObject meteor, Vector3 startPos, Vector3 endPos)
+    {
+        while (true)
+        {
+            float journeyLength = Vector3.Distance(startPos, endPos);
+            float startTime = Time.time;
+
+            while (Vector3.Distance(meteor.transform.position, endPos) > 0.1f)
+            {
+                float distCovered = (Time.time - startTime) * moveSpeed;
+                float fractionOfJourney = distCovered / journeyLength;
+                meteor.transform.position = Vector3.Lerp(startPos, endPos, fractionOfJourney);
+                yield return null;
+            }
+
+            meteor.transform.position = startPos;
+            RotateTo(meteor, endPos);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (moveSpeed != 0 && rb != null)
+        {
+            rb.position += transform.forward * (moveSpeed * Time.deltaTime);
         }
     }
 }
+
