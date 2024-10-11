080888644f7c6708b7382072a9b3e3699bb52efb
DoktorBamse <doktorbamse2620@gmail.com>
2024-10-11 19:33:09 +0200

    Added animation to stone spawning

--- Assets/Scripts/Objects/Obstacle2.cs
+++ Assets/Scripts/Objects/Obstacle2.cs
@@ -9,15 +9,18 @@ public class Obstacle2 : MonoBehaviour
 {
     // ----------------------------------------------------------------
 
-    public float updateTime = 3;
+    public float updateTime = 3f;
     public int amountIncrease = 3;
     public int amountDecrease = 4;
     public float distanceBetweenObjects = 6.5f;
-    public float distanceToPlayer = 4;
-    public float spawnRadiusIncrease = 1;
+    public float distanceToPlayer = 4f;
+    public float spawnHeightIncrease = 1f;
     public int maxObjects = 15;
     public int minObjects = 10;
     public int startAmount = 5;
+    public float spawnDepth = 0.2f;
+    public float rotateSpeed = 360f;
+    public float moveSpeed = 1f;
 
     public GameObject planet;
     public GameObject player;
@@ -25,10 +28,8 @@ public class Obstacle2 : MonoBehaviour
 
     // ----------------------------------------------------------------
 
-    private float minimumDistanceToPlayer;
     private float spawnDistanceFromPlanet;
     private float prefabWidth;
-    private float prefabHeight;
     private float time = 0;
     private bool obstacleIncrease = true;
 
@@ -36,24 +37,32 @@ public class Obstacle2 : MonoBehaviour
 
     private GameObject newObstacle;
     public List<GameObject> obstaclePrefabs = new List<GameObject>();
+    public List<GameObject> dyingPrefabs = new List<GameObject>();
+    private List<float> spawnOffset = new List<float>();
+    private List<float> deathOffset = new List<float>();
 
     // ----------------------------------------------------------------
 
     void Start()
     {
         prefabWidth = obstaclePrefab.transform.localScale.x;
-        prefabHeight = obstaclePrefab.transform.localScale.z / 2;
 
-        spawnDistanceFromPlanet = (planet.transform.localScale.x + spawnRadiusIncrease) / 2;
+        spawnDepth += 1f;
+
+        spawnDistanceFromPlanet = (planet.transform.localScale.x + spawnHeightIncrease) / 2;
 
         for (int i = 0; i < startAmount; i++)
         {
             spawnObject();
+            obstaclePrefabs[i].transform.position *= spawnOffset[i];
+            spawnOffset[i] = 1f;
         }
     }
 
     void Update()
     {
+        posUpdate();
+
         time += Time.deltaTime;
         if (time >= updateTime)
         {
@@ -83,8 +92,11 @@ public class Obstacle2 : MonoBehaviour
             for (int i = 0; i < Mathf.Round(time / updateTime) * amountDecrease; i++)
             {
                 int randomObject = Random.Range(0, obstaclePrefabs.Count - 1);
-                Destroy(obstaclePrefabs[randomObject]);
-                obstaclePrefabs.RemoveAt(randomObject);
+                dyingPrefabs.Add(obstaclePrefabs[randomObject]);
+                deathOffset.Add(1f);
+                obstaclePrefabs.RemoveAt(randomObject);;
+                spawnOffset.RemoveAt(randomObject);;
+
 
                 if (obstaclePrefabs.Count == 0)
                 {
@@ -109,10 +121,14 @@ public class Obstacle2 : MonoBehaviour
             Vector3 spawnPos = Random.onUnitSphere * spawnDistanceFromPlanet;
             if (spawnAttemptCheck(spawnPos))
             {
-                newObstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.identity);
+                newObstacle = Instantiate(obstaclePrefab, spawnPos / spawnDepth, Quaternion.identity);
+
                 newObstacle.transform.LookAt(planet.transform);
                 newObstacle.transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+
                 obstaclePrefabs.Add(newObstacle);
+                spawnOffset.Add(spawnDepth);
+
                 spawnSuccess = true;
             }
         }
@@ -129,10 +145,10 @@ public class Obstacle2 : MonoBehaviour
         {
             return false;
         }
-        
-        for(int i = 0; i < obstaclePrefabs.Count; i++)
+
+        foreach (GameObject obstaclePrefab in obstaclePrefabs)
         {
-            dist = Vector3.Distance(obstaclePrefabs[i].transform.position, spawnPos);
+            dist = Vector3.Distance(obstaclePrefab.transform.position, spawnPos);
 
             if (prefabWidth / 2 * 1.5f < dist && dist < distanceBetweenObjects)
             {
@@ -141,6 +157,67 @@ public class Obstacle2 : MonoBehaviour
         }
 
         return true;
+    }
+
+    // ----------------------------------------------------------------
+
+    private void posUpdate()
+    {
+        for (int i = 0; i < obstaclePrefabs.Count; i++)
+        {
+            Vector3 obstaclePos = obstaclePrefabs[i].transform.position;
+
+            if (spawnOffset[i] > 1f)
+            {
+                obstaclePos *= spawnOffset[i];
+
+                spawnOffset[i] -= moveSpeed * Time.deltaTime;
+
+                if (i % 2 == 0)
+                {
+                    obstaclePrefabs[i].transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+                } else
+                {
+                    obstaclePrefabs[i].transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+                }
+
+                if (spawnOffset[i] < 1f)
+                {
+                    spawnOffset[i] = 1f;
+                }
+
+                obstaclePos /= spawnOffset[i];
+            }
+
+            obstaclePrefabs[i].transform.position = obstaclePos;
+        }
+
+        for (int i = 0; i < dyingPrefabs.Count; i++)
+        {
+            if (deathOffset[i] < spawnDepth)
+            {
+                dyingPrefabs[i].transform.position *= deathOffset[i];
+
+                deathOffset[i] += moveSpeed * Time.deltaTime;
+
+                if (i % 2 == 0)
+                {
+                    dyingPrefabs[i].transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    dyingPrefabs[i].transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+                }
+
+                dyingPrefabs[i].transform.position /= deathOffset[i];
+
+            } else
+            {
+                Destroy(dyingPrefabs[i]);
+                dyingPrefabs.RemoveAt(i);
+                deathOffset.RemoveAt(i);
+            }
+        }
     }
 
     // ----------------------------------------------------------------
